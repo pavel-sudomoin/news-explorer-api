0801c3f8e9c18c8bcf16b98cfbb3853fb68947b8
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-04-29 22:41:40 +0300

    add global route

--- app.js
+++ app.js
@@ -6,17 +6,12 @@ const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
 const cors = require('cors');
 
-const { login, createUser } = require('./controllers/users.js');
-const { loginValidate, userValidate } = require('./validators/compiled-schemes');
-const { bodyValidator } = require('./validators/validators');
-const auth = require('./middlewares/auth.js');
-const articles = require('./routes/articles.js');
-const users = require('./routes/users.js');
-const wrongRequests = require('./routes/wrong-requests.js');
-const errorHandler = require('./middlewares/error-handler.js');
-const rateLimiter = require('./configs/rate-limiter');
+const errorHandler = require('./middlewares/error-handler');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { databaseUrl, databaseSettings } = require('./configs/database');
+const rateLimiter = require('./configs/rate-limiter');
+
+const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
 
@@ -32,10 +27,6 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(rateLimiter);
 app.use(requestLogger);
-app.post('/signin', bodyValidator(loginValidate), login);
-app.post('/signup', bodyValidator(userValidate), createUser);
-app.use('/articles', auth, articles);
-app.use('/users', auth, users);
-app.use('*', wrongRequests);
+app.use(router);
 app.use(errorLogger);
 app.use(errorHandler);
