61dc6020cd00e07ee62cb0530e6399bb83207856
Pavel Sudomoin <sudomoin.pavel@yandex.ru>
2020-05-27 20:48:12 +0300

    solve eslint problem with error-handler

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,6 +1,5 @@
 const messages = require('../configs/messages');
 
-// eslint-disable-next-line no-unused-vars
 module.exports = (err, req, res, next) => {
   const { message = messages.main.isServerError, statusCode = 500 } = err;
   res.status(statusCode).send({ message });
